c4f637c42dab6b6de615f4732bfcbb5e411ced97
Masterfoni <joaovitor_179@hotmail.com>
2017-11-25 19:51:20 -0200

    Criação de menu lateral para a homepage

--- app/src/main/java/br/edu/ifpe/tads/pdm/faljval/workaround/HomeActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/faljval/workaround/HomeActivity.java
@@ -1,7 +1,13 @@
 package br.edu.ifpe.tads.pdm.faljval.workaround;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -10,10 +16,12 @@ import com.google.firebase.auth.FirebaseUser;
 
 import br.edu.ifpe.tads.pdm.faljval.workaround.auth.FirebaseAuthListener;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth mAuth;
     private FirebaseAuthListener authListener;
+    private DrawerLayout menuDrawer;
+    private ActionBarDrawerToggle menuToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,37 @@ public class HomeActivity extends AppCompatActivity {
 
         this.mAuth = FirebaseAuth.getInstance();
         this.authListener = new FirebaseAuthListener(this);
+
+        menuDrawer = findViewById(R.id.homepage);
+        menuToggle = new ActionBarDrawerToggle(this, menuDrawer, R.string.menu_abrir, R.string.menu_fechar);
+        menuDrawer.addDrawerListener(menuToggle);
+        menuToggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (menuDrawer.isDrawerOpen(GravityCompat.START)) {
+            menuDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.nav_sair)
+        {
+            BtnSignOutClick(findViewById(R.id.homepage));
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.homepage);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
 
