19309ef42e94b8318b7a5a9a72cb5c8ac4ac4901
Felipe Lima <fal@a.recife.ifpe.edu.br>
2017-11-25 22:27:13 -0300

    Criação de HomeWork Tentativa de inserçao do mapa. Não completo

--- app/src/main/java/br/edu/ifpe/tads/pdm/faljval/workaround/HomeWorkerActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/faljval/workaround/HomeWorkerActivity.java
@@ -3,42 +3,32 @@ package br.edu.ifpe.tads.pdm.faljval.workaround;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import br.edu.ifpe.tads.pdm.faljval.workaround.auth.FirebaseAuthListener;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeWorkerActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     private FirebaseAuth mAuth;
     private FirebaseAuthListener authListener;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home_worker);
 
         this.mAuth = FirebaseAuth.getInstance();
         this.authListener = new FirebaseAuthListener(this);
-
-        Switch swt = (Switch) findViewById(R.id.swt_on);
-
-        swt.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                if(checked) {
-
-                } else {
-
-                }
-            }
-        });
     }
-
 
     public void BtnSignOutClick(View view) {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -48,6 +38,16 @@ public class HomeActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "ERROR!", Toast.LENGTH_SHORT);
         }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     @Override
