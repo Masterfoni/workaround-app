a0d6f45a1e623fe57dae727e4776c7e04375f18b
Masterfoni <joaovitor_179@hotmail.com>
2017-11-28 21:31:29 -0200

    Melhorias de layout e de validações

--- app/src/main/java/br/edu/ifpe/tads/pdm/faljval/workaround/SignUpUserActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/faljval/workaround/SignUpUserActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -21,9 +22,12 @@ public class SignUpUserActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuthListener authListener;
+
     private EditText edEmail;
     private EditText edSenha;
     private EditText edNome;
+
+    private Button botaoCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,8 @@ public class SignUpUserActivity extends AppCompatActivity {
         edEmail = (EditText) findViewById(R.id.edit_email);
         edSenha = (EditText) findViewById(R.id.edit_senha);
         edNome = (EditText) findViewById(R.id.edit_nome);
+
+        botaoCadastrar = (Button) findViewById(R.id.btn_cadastrar);
     }
 
     public void btnCadastrarUserClick(View view) {
@@ -43,6 +49,11 @@ public class SignUpUserActivity extends AppCompatActivity {
         final String email = edEmail.getText().toString();
         final String password = edSenha.getText().toString();
         final String nome = edNome.getText().toString();
+
+        if(!validar()) {
+            invalidarCadastro();
+            return;
+        }
 
         final FirebaseAuth mAuth = FirebaseAuth.getInstance();
         mAuth.createUserWithEmailAndPassword(email, password)
@@ -60,6 +71,49 @@ public class SignUpUserActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    public void invalidarCadastro() {
+        Toast.makeText(getBaseContext(), "Falha ao cadastrar", Toast.LENGTH_LONG).show();
+
+        botaoCadastrar.setEnabled(true);
+    }
+
+    public void validarCadastro() {
+        botaoCadastrar.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public boolean validar() {
+        boolean valido = true;
+
+        String nome = edNome.getText().toString();
+        String email = edEmail.getText().toString();
+        String senha = edSenha.getText().toString();
+
+        if (nome.isEmpty() || nome.length() < 3) {
+            edNome.setError("at least 3 characters");
+            valido = false;
+        } else {
+            edNome.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            edEmail.setError("enter a valid email address");
+            valido = false;
+        } else {
+            edEmail.setError(null);
+        }
+
+        if (senha.isEmpty() || senha.length() < 4 || senha.length() > 10) {
+            edSenha.setError("between 4 and 10 alphanumeric characters");
+            valido = false;
+        } else {
+            edSenha.setError(null);
+        }
+
+        return valido;
     }
 
     @Override
